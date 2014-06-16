8268d568f9d8585fdd7ef9b30e22e054391a7c1e
Christian Dobert <chr.dobert@gmail.com>
2014-06-16 14:16:22 +0200

    Initial commit

---
+++ lib/fullcalendar/wrapper/rails.rb
@@ -0,0 +1,10 @@
+require "fullcalendar/wrapper/rails/version"
+
+module Fullcalendar
+  module Wrapper
+    module Rails
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
---
+++ lib/fullcalendar/wrapper/rails/version.rb
@@ -0,0 +1,7 @@
+module Fullcalendar
+  module Wrapper
+    module Rails
+      VERSION = "2.0.1"
+    end
+  end
+end
